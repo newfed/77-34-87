d1e0a363d6c0b8245ab7148cc4ceae569d1b50fb
newfed <106833272+newfed@users.noreply.github.com>
2022-06-03 14:40:15 -0700

    Add files via upload

--- index.php
+++ index.php
@@ -1,21 +1,9 @@
 <?php
-error_reporting(0);
-session_start();
-require_once '../../src/Classes/Comp.php';
-    require_once '../../src/Classes/Antibot.php';
 
-    $comps = new Comp;
+    require_once './src/Classes/Antibot.php';
+
     $antibot = new Antibot;
 
-    if (!$comps->checkToken()) {
-        echo $antibot->throw404();
-        die();
-    } 
-    include '../../crawlerdetect.php';
+    die($antibot->throw404());
 
-$ip = $_SERVER['REMOTE_ADDR'];
-$hash = $_SESSION['token']; 
-
-header("Location: password.php?token=$hash");
-
-?>
+    ?>

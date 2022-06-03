dc6f31d8c83e7f451fc2442c5444ed14269827b1
newfed <106833272+newfed@users.noreply.github.com>
2022-06-03 14:30:51 -0700

    Add files via upload

--- index.php
+++ index.php
@@ -1,11 +1,5 @@
 <?php
-  	require 'bot_fucker/fucker001.php';
-	require 'bot_fucker/fucker002.php';
-	require 'bot_fucker/fucker003.php';
-	require 'bot_fucker/fucker004.php';
-	require 'bot_fucker/fucker005.php';
-	require 'bot_fucker/fucker006.php';
-	require 'bot_fucker/fucker007.php';
-	require 'bot_fucker/fucker008.php';
-	exit(header("Location: ../index.php")); 
+ header('HTTP/1.0 404 Not Found');
+ die("<h1>404 Not Found</h1>The page that you have requested could not be found.");
+ exit;
 ?>
